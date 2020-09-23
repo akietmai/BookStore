162a86a7bea81aedf7498371ac7de6f8bfddddfc
akietmai <akietmai@gmail.com>
2020-09-23 15:26:14 +0700

    Initial Author Routes

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
+const authorsRouter = require('./routes/authors');
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // mongoose 2vcd3prrIloBDKMV
 const mongoose = require('mongoose');
@@ -21,5 +24,6 @@ mongoose.connect(
 );
 
 app.use('/', indexRouter);
+app.use('/authors', authorsRouter);
 
 app.listen(process.env.PORT || 3000);
